3ccd97da7b88c4bc543ee6d53ac62962febc1a4c
KepLer <kepler007@mail.ru>
2020-04-05 01:52:08 +0400

    Cliker whatsapp with clipboard function (win 10)

--- test.py
+++ test.py
@@ -1,28 +1,24 @@
 import pyautogui
+import time
+import pyperclip
 
-pyautogui.click(25, 1060)
-pyautogui.PAUSE = 1
-pyautogui.click(590, 850)
-pyautogui.PAUSE = 1
-pyautogui.click(590, 850)
-pyautogui.PAUSE = 2
-pyautogui.click(500, 50)
+pyautogui.click(25, 1060)   # win button
+time.sleep(2)
+pyautogui.click(590, 850)   # chrome button
+time.sleep(2)
+pyautogui.click(500, 50)    # address string
 pyautogui.typewrite('https://web.whatsapp.com/')
-pyautogui.PAUSE = 2
-pyautogui.keyDown('Enter')
-pyautogui.PAUSE = 10
-
-pyautogui.click(500, 200)
-pyautogui.PAUSE = 1
-pyautogui.click(500, 200)
-
-#pyautogui.click(500, 300)
-pyautogui.typewrite('шай')
-pyautogui.PAUSE = 2
-pyautogui.click(500, 420)
-pyautogui.PAUSE = 2
-
-#pyautogui.click(100,990)
-pyautogui.typewrite('test тест 123')
-pyautogui.PAUSE = 2
-#pyautogui.click(1625,990)
+time.sleep(2)
+pyautogui.keyDown('Enter')  # emulate Enter button
+time.sleep(20)
+pyautogui.click(500, 300)   # search string
+time.sleep(2)
+pyautogui.typewrite('shaik')
+time.sleep(2)
+pyautogui.click(500, 420)   # select group
+time.sleep(2)
+pyperclip.copy('test тест ололо 123')   # input text into clipboard
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'v')   # emulate Ctrl + V
+time.sleep(2)
+#pyautogui.click(1625,990)  # send button
