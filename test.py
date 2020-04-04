07fcd9e6055fdef77bc141cdbf990660462e0f06
KepLer <kepler007@mail.ru>
2020-04-04 17:45:40 +0400

    Controle x,y only inside created window

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("Hallo!")
+from tkinter import *
+
+def move(event):
+    x = root.winfo_pointerx() - root.winfo_rootx()
+    y = root.winfo_pointery() - root.winfo_rooty()
+    test = str(x) + " " + str(y)
+    #test = root.winfo_pointerxy()
+    l['text'] = test
+
+
+# 1625 / 991
+root = Tk()
+l = Label(bg='white', fg='black', width=50)
+l.pack()
+root.bind('<Motion>', move)
+root.mainloop()
+
+
+
+
+#btn = Button(root, text="Click me", width=30,height=5, bg="white",fg="black")
+#btn.bind("<Button-1>", Hello)
+#btn.pack()
+
+#Button(root, text = '1').pack(side = 'left')
