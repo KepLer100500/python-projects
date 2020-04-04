61879940325f7bf51acef5d814db1655735db5b0
KepLer <kepler007@mail.ru>
2020-04-05 01:07:07 +0400

    Cliker whatsapp open chrome and click coordinates in dialog

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
 import pyautogui
 
-pyautogui.click(100,990)
-pyautogui.typewrite('test')
-pyautogui.click(100,990)
-pyautogui.click(1625,990)
+pyautogui.click(25, 1060)
+pyautogui.PAUSE = 1
+pyautogui.click(590, 850)
+pyautogui.PAUSE = 1
+pyautogui.click(590, 850)
+pyautogui.PAUSE = 2
+pyautogui.click(500, 50)
+pyautogui.typewrite('https://web.whatsapp.com/')
+pyautogui.PAUSE = 2
+pyautogui.keyDown('Enter')
+pyautogui.PAUSE = 10
+
+pyautogui.click(500, 200)
+pyautogui.PAUSE = 1
+pyautogui.click(500, 200)
+
+#pyautogui.click(500, 300)
+pyautogui.typewrite('шай')
+pyautogui.PAUSE = 2
+pyautogui.click(500, 420)
+pyautogui.PAUSE = 2
+
+#pyautogui.click(100,990)
+pyautogui.typewrite('test тест 123')
+pyautogui.PAUSE = 2
+#pyautogui.click(1625,990)
