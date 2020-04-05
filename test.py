b3401109f6dcad09b8c3bb5782f4cf53f78362a3
KepLer <kepler007@mail.ru>
2020-04-05 22:08:58 +0400

    Cliker whatsapp with clipboard function (raspbery pi 4 screen 1600x900)

--- test.py
+++ test.py
@@ -2,23 +2,23 @@ import pyautogui
 import time
 import pyperclip
 
-pyautogui.click(25, 1060)   # win button
+#pyautogui.click(25, 1060)   # win button
 time.sleep(2)
-pyautogui.click(590, 850)   # chrome button
-time.sleep(2)
-pyautogui.click(500, 50)    # address string
+pyautogui.click(100, 20)   # chrome button
+time.sleep(5)
+pyautogui.click(300, 120)    # address string
 pyautogui.typewrite('https://web.whatsapp.com/')
-time.sleep(2)
+time.sleep(5)
 pyautogui.keyDown('Enter')  # emulate Enter button
-time.sleep(20)
-pyautogui.click(500, 300)   # search string
-time.sleep(2)
+time.sleep(23)
+pyautogui.click(300, 330)   # search string
+time.sleep(3)
 pyautogui.typewrite('shaik')
-time.sleep(2)
-pyautogui.click(500, 420)   # select group
-time.sleep(2)
+time.sleep(3)
+pyautogui.click(300, 450)   # select group
+time.sleep(3)
 pyperclip.copy('test тест ололо 123')   # input text into clipboard
-time.sleep(2)
+time.sleep(3)
 pyautogui.hotkey('ctrl', 'v')   # emulate Ctrl + V
-time.sleep(2)
-#pyautogui.click(1625,990)  # send button
+time.sleep(3)
+pyautogui.click(1470,850)  # send button
